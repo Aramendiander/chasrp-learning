21c3a642bf56779b23d093610881dcc6e28f6319
Ander Aramendi <aramendiander@gmail.com>
2024-02-20 00:31:05 +0100

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace chasrp_learning.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
